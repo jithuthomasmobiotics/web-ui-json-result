bde75254f4339becacd6a12cccc8b1ddec3e5c1b
jithuthomasmobiotics <jithu.thomas@mobiotics.com>
2017-07-07 20:46:04 +0530

    Create test.php

---
+++ test.php
@@ -0,0 +1,46 @@
+<?php
+/*********************************************************
+   Author:      Niyas T
+   Copyright:   Mobiotics IT Solution Private Limited
+   Version:     1.0
+   Date:        1-Oct-2015
+
+   FileName:    login.php
+   Description: VBOXLITE Vendor Account Login
+**********************************************************/
+header("Content-Type:application/json");
+error_reporting(E_ALL);
+
+
+require_once('helpers.php');
+require_once('utilities.php');
+
+
+//First check what is the method
+if(!isset($_SERVER['REQUEST_METHOD']) || !isset($_SERVER['REQUEST_URI']))
+{
+
+        HTTPFailWithCode(400,'HTTP Method or request URI is not set');
+}
+
+$method = $_SERVER['REQUEST_METHOD'];
+$request = $_SERVER['REQUEST_URI'];
+
+if($method!='GET')
+{
+        HTTPFailWithCode(405,'HTTP Method not allowed');
+
+}
+
+
+$parts = parse_url($request);
+$path_parts = pathinfo($parts['path']);
+$id = $path_parts['filename'];
+
+$result = getResponseResult($id,$_GET);
+
+
+print json_encode($result);
+
+exit(0);
+?>
